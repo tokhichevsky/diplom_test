8ce74902437aaeabdd44c7440c98a5edaaac6e64
Anton Tarasov <atarasov@emphasoft.com>
2021-05-02 15:30:18 +0300

    Added notation.

--- src/components/Screen/screens/TestInfo1/TestInfo1.tsx
+++ src/components/Screen/screens/TestInfo1/TestInfo1.tsx
@@ -1,7 +1,8 @@
+import Instruction from "components/Test/Instruction/Instruction";
+import {ScreenTypes} from "models/Screen.model";
+import GoButton from "../../../GoButton/GoButton";
 import CenterText from "../../../UI/CenterText/CenterText";
 import Navigation from "../../../UI/Navigation/Navigation";
-import {ScreenTypes} from "../../../../models/Screen.model";
-import GoButton from "../../../GoButton/GoButton";
 
 const TestInfo1 = () => {
   return (
@@ -14,12 +15,17 @@ const TestInfo1 = () => {
           устройство). Использование любого из этих устройств не только подпортит данные нашего эксперимента, но и будет
           является абсолютно бессмысленным относительно Вашей выгоды от участия.</p>
 
+        <Instruction>
+          <b>Мы бы хотели еще раз поставить акцент на то, что Вам НЕ (!!!) нужно считать время, даже про себя.
+            Исследование посвящено процессу ВОСПРИЯТИЯ времени, а не способности концентрироваться на счете.</b>
+        </Instruction>
+
         <p>Пожалуйста, выключите звук и любые уведомления на всех своих устройствах прежде чем убрать их. Любое
           уведомление отвлечет Вас. Данная просьба лишь еще раз подчеркивает, насколько нам важно Ваше внимание в это
           время.</p>
       </CenterText>
       <Navigation>
-        <div />
+        <div/>
         <GoButton to={ScreenTypes.TestInfo2}>
           Понятно
         </GoButton>
